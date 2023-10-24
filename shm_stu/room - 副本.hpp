248a5e6bdbd6b63d6c1948f8fa544bb38c1c3ed0
kate <2849876010@qq.com>
2023-10-24 15:12:10 +0800

    添加新文件

---
+++ shm_stu/room - 副本.hpp
@@ -0,0 +1,20 @@
+#pragma one
+#include <iostream>
+#include <cstring>
+#define KEY 12435
+
+class room
+{
+    public:
+        int id;
+        int number;
+         
+    public:
+        room(int _id=0,int _number=0)
+            :id(_id),number(_number)
+            {
+               
+               
+            }
+};
+
